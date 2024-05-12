490ace26ebeed630b7cfda5426ba71e5ca46eca1
voronov <114691383+voronov-nikita@users.noreply.github.com>
2024-05-13 00:01:34 +0300

    add main files and change any files from old repo

---
+++ App.js
@@ -0,0 +1,8 @@
+import { HomeScreen } from './pages/Home'
+import React from 'react'
+
+const App = () => {
+    return <HomeScreen />
+}
+
+export default App
---
+++ pages/StylesPage.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export const homeStyle = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	heading: {
+		fontSize: 24,
+		marginBottom: 20,
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		width: '80%',
+	},
+})
