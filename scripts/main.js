a14aaa5ee2b00a3574841360d47b875a663781c2
voronov-nikita <voronovnr_1@mail.ru>
2025-01-12 19:39:23 +0300

    update files

---
+++ scripts/main.js
@@ -0,0 +1,58 @@
+const ctx = document
+    .getElementById('myChart')
+    .getContext('2d');
+const myChart = new Chart(ctx, {
+    // Тип диаграммы (столбчатая)
+    type: 'bar', 
+    data: {
+        labels: [
+            "Python", "JS", "C++", "Kotlin", "Dart", "Other"
+        ],
+        datasets: [
+            {
+                data: [1, 2, 3, 4, 5, 6],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 205, 86, 0.6)',
+                    'rgba(250, 25, 74, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(188, 41, 149, 0.6)',
+                    'rgba(74, 71, 68, 0.6)',
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 205, 86, 1)',
+                    'rgba(250, 25, 74, 1)', 
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(188, 41, 149, 1)',
+                    'rgba(74, 71, 68, 1)',
+                ],
+                // Ширина обводки
+                borderWidth: 2,
+            },
+        ],
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    },
+    options: {
+        // Переворот диаграммы на 90 градусов
+        indexAxis: 'y',
+        responsive: true,
+        scales: {
+            x: {
+                beginAtZero: true,
+                grid: {
+                    color: '#333'
+                },
+            },
+            y: {
+                grid: {
+                    color: '#333',
+                },
+            },
+        },
+    },
+});
